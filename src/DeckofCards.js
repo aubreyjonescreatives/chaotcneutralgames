7b908656bfd97f5514141d6a9a916b402c20c6dc
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-03-30 14:28:10 -0600

    fixed warnings

--- src/DeckofCards.js
+++ src/DeckofCards.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import axios from 'axios'
 import {Card, IconButton, CardMedia, Typography, Container, 
     Dialog, Button, DialogTitle, DialogContent, DialogContentText, 
